83e04fffb1d88c86d6d5caf435b26ac97ae3da08
Ivan <IvanBWork@yandex.ru>
2024-08-20 01:16:08 +0300

    Добавьте файлы проекта.

---
+++ HomeworkCSharp02/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace HomeworkCSharp02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] a = { { 7, 3, 2 },
+                         { 4, 9, 6 },
+                         { 1, 8, 5 } };
+
+            int[] arraySort = new int[a.Length];
+            var counter = 0;
+            for (int i = 0; i < a.GetLength(0); i++)
+            {
+                for (int j = 0; j < a.GetLength(1); j++)
+                {
+                    arraySort[counter++] = a[i, j];
+                }
+            }
+            Array.Sort(arraySort);
+            counter = 0;
+            for (int i = 0; i < a.GetLength(0); i++)
+            {
+                for (int j = 0; j < a.GetLength(1); j++)
+                {
+                    a[i, j] = arraySort[counter++];
+                    Console.Write(a[i, j]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
